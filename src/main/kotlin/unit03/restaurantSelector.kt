8418606c3f7b2679b07e0e1b3ab76a3bc87b96c5
AllisonMarieLee <alli88lee@gmail.com>
2023-02-18 19:41:09 -0600

    Created a new project

---
+++ src/main/kotlin/unit03/restaurantSelector.kt
@@ -0,0 +1,19 @@
+fun main(){
+    var vegetarian= "yes"
+    println("Vegetarian? $vegetarian")
+
+    var vegan= "yes"
+    println("Vegan? $vegan")
+
+    var glutenFree= "no"
+    println("Gluten-Free? $glutenFree")
+
+    if (vegetarian=="yes")
+        if (vegan=="yes")
+            if (glutenFree=="yes")
+                println("Corner Cafe, Chef's Kitchen")
+            else
+                println("Corner Cafe, Chef's Kitchen")
+        else
+            else (glutenFree=="yes")
+}               println("Main Street, Corner Cafe, Chef's Kitchen")
