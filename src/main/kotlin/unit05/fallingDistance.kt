5374ab58a918b6a984407887e07763f42faaafb2
AllisonMarieLee <alli88lee@gmail.com>
2023-03-30 21:24:01 -0500

    Created a new project

---
+++ src/main/kotlin/unit05/fallingDistance.kt
@@ -0,0 +1,16 @@
+fun main(){
+    var time = 1.0
+    while (time <= 10.0) {
+        val distance = fallingDistance(time.toDouble())
+        println("Time(sec): $time, Distance(meter): $distance")
+        time++
+    }
+}
+
+fun fallingDistance(time: Double): Double {
+    //d=1/2(gt^2) formula, d=distance(meters), g=9.8, t=time(seconds--from user input)
+    val exponent=2.0
+    val result=Math.pow(time, exponent)
+    val distance: Double= 0.5*(9.8 * result)
+    return distance
+}
